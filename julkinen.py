be6647778b2f3b690a6d2be44c221317e9368e82
Ohto Nordberg <ohto.nordberg@edu.ii.fi>
2022-02-27 20:23:37 +0200

    bugittaa

--- julkinen.py
+++ julkinen.py
@@ -2,5 +2,6 @@ print("eka julkinen koodinpätkä!")
 <<<<<<< HEAD
 print("Nyt on muokattu koodia paikallisesti")
 print("Toivottavasti toimii!!")
+print("jestas ko bugittaa")
 =======
 >>>>>>> f2893a92111aa54a375fcb04aabf061a3c0ac3f4
